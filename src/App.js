b030819e0fb5fa931a86cf332b2f22a516e67707
Zhoha Damani <zhohadamani@gmail.com>
2020-11-27 12:52:46 +0530

    Configuring React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+//importing the components from reactstrap to create the navbar
+import { Navbar, NavbarBrand } from 'reactstrap';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+    return (
+        <div className="App">
+            {/* for dark navbar, here you write it as below, if you want other color, try primary, or other */}
+       <Navbar color="dark" className="navbar-dark">
+        <div className="container">
+        <NavbarBrand href="/">  Ristorante Con Fusion</NavbarBrand>
+        </div>
+       </Navbar>
+      </div>
+    );
+} export default App;
